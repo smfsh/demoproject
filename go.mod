361bd8d22498bb7ca6524a5dbd3f973139026632
Miles McLean <Mills00013@gmail.com>
2020-07-23 12:46:43 -0500

    Added image output to our tool

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/smfsh/demoproject
+
+go 1.14
+
+require github.com/eliukblau/pixterm v1.3.1
--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image/color"
+
+	"github.com/eliukblau/pixterm/pkg/ansimage"
+)
+
+var bg = color.RGBA{
+	R: 0,
+	G: 0,
+	B: 0,
+	A: 0,
+}
+var dm ansimage.DitheringMode = 0
+var sm ansimage.ScaleMode = 0
 
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("My name is Miles")
+
+	snyk := getImage("https://theme.zdassets.com/theme_assets/9132644/8fea1d21f84207552082e1091daebd33032edd75.png")
+	fmt.Print(snyk)
+}
+
+func getImage(url string) string {
+	image, err := ansimage.NewScaledFromURL(url, 60, 60, bg, sm, dm)
+	if err != nil {
+		panic(err)
+	}
+	return image.Render()
 }
